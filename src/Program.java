c9a8f5b18908b00e471ebc6e4f8739acbf54075a
Ivan Murzak <ivan.d.murzak@gmail.com>
2017-09-01 20:11:51 +0300

    Обновлен MediaPlayer пример

--- src/Program.java
+++ src/Program.java
@@ -1,16 +1,19 @@
 import com.alibaba.fastjson.JSON;
 import entity.Fruit;
+import javafx.application.Application;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.net.URI;
 import java.util.Scanner;
 
-public class Program {
+public class Program extends Application {
     private static final String[] SONGS = new String[] {
-        "https://www.mfiles.co.uk/mp3-downloads/Mozart-minuet-k2.mp3"
+        "https://www.mfiles.co.uk/mp3-downloads/Mozart-minuet-k2.mp3",
+        "https://www.mfiles.co.uk/mp3-downloads/sonata-in-c.mp3"
     };
     private static int currentSong = 0;
 
@@ -32,7 +35,14 @@ public class Program {
         }
     };
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException, InterruptedException {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.show();
+
         initMediaPlayer(0);
         player.play();
     }
